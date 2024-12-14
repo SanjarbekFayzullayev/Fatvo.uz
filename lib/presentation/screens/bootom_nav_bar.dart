1664b7e60f101e397fbe7b56f73d36a99f3bc0de
SanjarbekFayzullayev <121254411+SanjarbekFayzullayev@users.noreply.github.com>
2024-12-14 16:48:57 +0500

    Alder remove

--- lib/presentation/screens/bootom_nav_bar.dart
+++ lib/presentation/screens/bootom_nav_bar.dart
@@ -31,86 +31,60 @@ class _BottomNavBarState extends State<BottomNavBar> {
   ];
 
 
-  Future<bool> _onWillPop() async {
-    final shouldExit = await showDialog(
-      context: context,
-      builder: (context) =>
-          AlertDialog(
-            title: const Text('Ilovadan chiqmoqchimisiz?'),
-            content: const Text(""),
-            actions: [
-              OutlinedButton(
-                onPressed: () => Navigator.of(context).pop(false), // Rad etish
-                child: const Text(
-                  'Yo\'q', style: TextStyle(color: Colors.blueGrey),),
-              ),
-              OutlinedButton(
-                onPressed: () => Navigator.of(context).pop(true), // Tasdiqlash
-                child: const Text(
-                  'Ha', style: TextStyle(color: Colors.blueGrey),),
-              ),
-            ],
-          ),
-    );
 
-    return shouldExit ?? false;
-  }
 
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: _onWillPop,
-      child: BlocProvider(
-        create: (context) => FatwasCubit(),
-        child: BlocConsumer<FatwasCubit, FatwasState>(
-          listener: (context, state) {
-            // TODO: implement listener
-          },
-          builder: (context, state) {
-            return Scaffold(
-              bottomNavigationBar: SalomonBottomBar(
-                backgroundColor: Colors.white,
-                unselectedItemColor: Colors.grey,
-                currentIndex: _currentIndex,
-                onTap: (index) => setState(() => _currentIndex = index),
-                items: [
+    return  BlocProvider(
+      create: (context) => FatwasCubit(),
+      child: BlocConsumer<FatwasCubit, FatwasState>(
+        listener: (context, state) {
+          // TODO: implement listener
+        },
+        builder: (context, state) {
+          return Scaffold(
+            bottomNavigationBar: SalomonBottomBar(
+              backgroundColor: Colors.white,
+              unselectedItemColor: Colors.grey,
+              currentIndex: _currentIndex,
+              onTap: (index) => setState(() => _currentIndex = index),
+              items: [
 
-                  /// Home
-                  SalomonBottomBarItem(
-                    icon: const Icon(Icons.home, size: 26,),
-                    title: const Text(
-                      "Bosh menyu",
-                      style: TextStyle(color: Color(0xff3F666B)),
-                    ),
-                    selectedColor: const Color(0xff19505D),
+                /// Home
+                SalomonBottomBarItem(
+                  icon: const Icon(Icons.home, size: 26,),
+                  title: const Text(
+                    "Bosh menyu",
+                    style: TextStyle(color: Color(0xff3F666B)),
                   ),
+                  selectedColor: const Color(0xff19505D),
+                ),
 
-                  /// Fatwas
-                  SalomonBottomBarItem(
-                    icon: const Icon(Icons.messenger, size: 20,),
-                    title: const Text(
-                      "Fatvolar",
-                      style: TextStyle(color: Color(0xff3F666B)),
-                    ),
-                    selectedColor: const Color(0xff3F666B),
+                /// Fatwas
+                SalomonBottomBarItem(
+                  icon: const Icon(Icons.messenger, size: 20,),
+                  title: const Text(
+                    "Fatvolar",
+                    style: TextStyle(color: Color(0xff3F666B)),
                   ),
+                  selectedColor: const Color(0xff3F666B),
+                ),
 
-                  /// Profile
-                  SalomonBottomBarItem(
-                    icon: const Icon(Icons.access_time_filled_rounded, size: 26,),
-                    title: const Text(
-                      "Namoz vaqtlari",
-                      style: TextStyle(color: Color(0xff3F666B)),
-                    ),
-                    selectedColor: const Color(0xff3F666B),
+                /// Profile
+                SalomonBottomBarItem(
+                  icon: const Icon(Icons.access_time_filled_rounded, size: 26,),
+                  title: const Text(
+                    "Namoz vaqtlari",
+                    style: TextStyle(color: Color(0xff3F666B)),
                   ),
-                ],
-              ),
-              body: _pages2[_currentIndex],
-            );
-          },
-        ),
+                  selectedColor: const Color(0xff3F666B),
+                ),
+              ],
+            ),
+            body: _pages2[_currentIndex],
+          );
+        },
       ),
     );
   }
